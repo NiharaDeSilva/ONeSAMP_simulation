88e98535097fcb7f7c0e606799b456e3110a0f2f
NiharaDeSilva <suhashi.desilva@gmail.com>
2025-01-13 00:16:56 -0500

    Script updates

--- refactor/release/driver2.sh
+++ refactor/release/driver2.sh
@@ -162,8 +162,7 @@ export log_file="execution_stats_original"
 if [ $1 ]; then
   if [ $1 == "generateSimulatedPopulationsOfKnownNe" ]; then
     log_file="simulation_log.txt"
-    echo "Simulation Log" > $log_file
-    echo "------------------------" >> $log_file
+    echo "Ne,Iteration,Time,Memory" > $log_file
     
     loci_values=(160) # Example loci values
     size_values=(100) # Example size values
@@ -197,17 +196,27 @@ if [ $1 ]; then
             # Start timing in milliseconds
             start_time=$(date +%s%3N)
             
-            nice -n $processPriority ionice -n $filePriority $ONESAMP2 -t1 -rC -b$numPOP -d1 -u$mutationRate -v${theta} -$microsatsOrSNPs -l$loci -i$outputSampleSize -o1 -f$ONESAMP2COAL_MINALLELEFREQUENCY -p > $OUTPUT$suffix
+            pid=$(./ONESAMP2 -t1 -rC -b$numPOP -d1 -u$mutationRate -v${theta} -$microsatsOrSNPs -l$loci -i$outputSampleSize -o1 -f$ONESAMP2COAL_MINALLELEFREQUENCY -p > $OUTPUT$suffix & echo $!)
+
+            total_memory=0
+
+            # Continuously check memory usage while the process is running
+            while kill -0 $pid 2>/dev/null; do
+              current_memory=$(ps -o rss= -p $pid)
+              total_memory=$((total_memory + current_memory))
+              sleep 0.1
+            done
 
             # End timing in milliseconds
             end_time=$(date +%s%3N)
             execution_time=$((end_time - start_time))
+            execution_time_sec=$(echo "scale=3; $execution_time_ms / 1000" | bc)
             
-            # Extract memory usage
-            max_memory=$(grep 'Maximum resident set size' temp_memory_usage.txt | awk '{print $6}')
+            # Convert total memory from KB to MB for better readability
+            total_memory_mb=$((total_memory / 1024))
 
             # Write to log file
-            echo "$iteration, Ne = $numPOP, Loci = $loci, Size = $outputSampleSize, $execution_time ms, $max_memory KB" >> $log_file
+            echo "$numPOP,$iteration,$execution_time_sec,$total_memory_mb" >> $log_file
 
             echo "Trial complete"
           done # End of size loop
